aab2ddb820b087014e5500bffb1530a38c011996
Anton Kurako <goodforgod.dev@gmail.com>
2017-10-16 13:17:08 +0300

    - Basic Model Service impl

---
+++ src/main/java/io/spring/guide/service/modelbased/IBasicModelService.java
@@ -0,0 +1,31 @@
+package io.spring.guide.service.modelbased;
+
+import io.spring.guide.model.BasicModel;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * "default comment"
+ *
+ * @author Anton Kurako (anku0817)
+ * @since 12.10.2017
+ */
+public interface IBasicModelService<T extends BasicModel<ID>, ID extends Serializable> {
+
+    boolean exist(ID id);
+
+    boolean exist(T t);
+
+    T find(ID id);
+
+    T save(T t);
+
+    List<T> save(List<T> list);
+
+    void remove(T t);
+
+    void remove(ID id);
+
+    void remove(List<T> list);
+}
