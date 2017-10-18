620e49a1e4a4a795cde06d5828740c813fbde171
Anton Kurako <goodforgod.dev@gmail.com>
2017-10-18 12:46:28 +0300

    - Contract impl - yml improvements

--- microservice-sql/src/test/java/resources/contracts/TransFindAll.groovy
+++ microservice-sql/src/test/java/resources/contracts/TransFindAll.groovy
@@ -1,4 +1,24 @@
 package contracts
 
-class TransFindAll {
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description(""" 
+Transaction info representation
+""")
+    request {
+        method 'GET'
+        url '/api/trans/all'
+    }
+
+    response {
+        status 200
+        body([
+                amount: regex("[1-9]{1,5}"),
+                address: regex("[1-9]{1,5}")
+        ])
+        headers {
+            contentType(applicationJson())
+        }
+    }
 }
